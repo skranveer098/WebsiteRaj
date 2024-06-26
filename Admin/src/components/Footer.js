65e6ff4cf31c94f2bbe9ff0ae6adcf25dced8c44
Rajni Singh <rajnisingh9031@gmail.com>
2024-06-26 13:42:00 +0530

    updated code

--- Admin/src/components/Footer.js
+++ Admin/src/components/Footer.js
@@ -6,7 +6,6 @@ const Footer = () => {
   return (
     <footer id="footer" className="footer">
       <div className="copyright">
-        {/* &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved */}
       </div>
       <div className="credits">
         {/* All the links in the footer should remain intact. */}
